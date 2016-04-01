a45ad85a7a9add9ddbc6ffde12a6e95ec24a9664
Chad <mscott4@my.apsu.edu>
2016-03-31 22:12:56 -0500

    Added list activity

--- app/src/main/java/edu/apsu/csci/projectpaint/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/projectpaint/MainActivity.java
@@ -1,16 +1,29 @@
 package edu.apsu.csci.projectpaint;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
+
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        ArrayList<String> items = new ArrayList<>();
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        setListAdapter(adapter);
     }
 
     @Override
@@ -28,7 +41,11 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_add) {
+
+            Intent intent = new Intent(this, PaintActivity.class);
+            startActivity(intent);
+
             return true;
         }
 
