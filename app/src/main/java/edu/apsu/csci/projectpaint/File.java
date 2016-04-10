2d50faa3da78a98a660633e7210b68033ba446ed
Chad <mscott4@my.apsu.edu>
2016-04-10 17:23:35 -0500

    Icons and resources added

---
+++ app/src/main/java/edu/apsu/csci/projectpaint/File.java
@@ -0,0 +1,25 @@
+package edu.apsu.csci.projectpaint;
+
+/**
+ * Created by Chad on 3/31/16.
+ */
+public class File {
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
